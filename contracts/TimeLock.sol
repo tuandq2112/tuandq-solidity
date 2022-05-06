f053db42dcde1a204907445ee086276bc8957a69
duong.nt <myusername@myemaildomain.com>
2022-05-06 15:41:44 +0700

    ISCHAIN-146 Thêm comment code phần vesting schedule

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -10,17 +10,15 @@ contract TokenTimelock is Ownable {
 
   IERC20 private _token;
 
-  //list vestor
   address[] private _beneficiary;
 
   struct amountData {
     uint256 amountReceive;
     uint256 amountReceived;
   }
-
-  // amount for vestor
   mapping(address => amountData) private _amount;
-  uint256 private _sumAmount;
+  uint256 public _sumAmount;
+  uint256 private _sumBalanceContractInTimes = 0;
 
   struct timesAndRate {
     uint256 timeLock;
@@ -28,33 +26,51 @@ contract TokenTimelock is Ownable {
   }
   uint256 private _times;
   mapping(uint256 => timesAndRate) private _timesAndRate;
-
   uint256 private _timeStart;
+
   uint256 private _count = 0;
   bool private _status = false;
+  uint256 private _statusContract = 0;
+
+  event SetBeneficiaryAmounts(
+    IERC20 token_,
+    address[] beneficiary_,
+    uint256[] amounts_
+  );
+  event SetTimesAndRate(uint256[] rate_, uint256[] timeLock_);
 
   constructor() {}
 
-  // admin thiet lap danh sach va so luong amoun cho tung address
+/**
+  * set dia chi token erc20 '_token'
+  * set danh sach cac address vestor '_beneficiary'
+  * set so luong token cho vestor '_amounts'
+ */
   function setBeneficiaryAmounts(
     IERC20 token_,
     address[] memory beneficiary_,
     uint256[] memory amounts_
   ) public onlyOwner {
     require(beneficiary_.length != 0, "beneficiary_.length >= 1");
-    require(amounts_.length != 0, "amounts_.length >= 1");
     require(beneficiary_.length == amounts_.length, "must be the same length");
+    require(_statusContract == 0 || _statusContract == 2, "not time to set");
 
     _token = token_;
+    _statusContract = 0;
 
     for (uint256 i = 0; i < beneficiary_.length; i++) {
       _amount[beneficiary_[i]].amountReceive = amounts_[i];
       _sumAmount = _sumAmount + amounts_[i];
       _beneficiary.push(beneficiary_[i]);
     }
+    emit SetBeneficiaryAmounts(token_, beneficiary_, amounts_);
   }
 
-  //admin thiet lap so lan vesting, ti le moi lan vesting và thoi gian moi lan vesting
+/**
+  * set so lan vesting '_times'
+  * set ti le moi lan vesting 'rate'
+  * set time lock moi lan vesting 'timelockk')
+ */
   function setTimesAndRate(
     uint256 times_,
     uint256[] memory rate_,
@@ -63,7 +79,7 @@ contract TokenTimelock is Ownable {
     require(times_ != 0, "times_ >= 1");
     require(times_ == rate_.length, "must be equal");
     require(times_ == timeLock_.length, "must be equal");
-    require(_count >= _times, "still in vesting time");
+    require(_statusContract == 0 || _statusContract == 2, "not time to set");
 
     _times = times_;
     uint256 sum = 0;
@@ -73,116 +89,81 @@ contract TokenTimelock is Ownable {
       _timesAndRate[i].timeLock = timeLock_[i];
     }
     require(sum == 100, "sum equal to 100%");
+    emit SetTimesAndRate(rate_, timeLock_);
   }
 
-  //admin goi khi bat dau tinh thoi gian vesting cho tung vong
+/**
+  * truoc khi goi ham startRelease() lan dau, balanceOf(address(this)) >= _sumAmount
+  *  
+ */
   function startRelease() public onlyOwner {
+    if (_count == 0) {
+      uint256 amount = token().balanceOf(address(this));
+      require(amount >= _sumAmount, "not enough tokens to release");
+    }
+    require(_count < _times, "too many times vesting");
     require(_status == false, "not start time");
     _timeStart = _timesAndRate[_count].timeLock + block.timestamp;
     _status = true;
+    _statusContract = 1;
   }
 
-  // tra ve dia chi token erc20
-  function token() public view virtual returns (IERC20) {
-    return _token;
-  }
-
-  // tra ve luot vesting dang thuc hien
-  function getCurrentTimes() public view virtual returns (uint256) {
-    return _count;
-  }
-
-  //tra ve danh sach cac dia chi vestor
-  function getListVestor() public view virtual returns (address[] memory) {
-    return _beneficiary;
-  }
-
-  //tra ve amount ung voi address vestor
-  function getAmountForBeneficiary(address beneficiary_)
-    public
-    view
-    virtual
-    returns (uint256)
-  {
-    return _amount[beneficiary_].amountReceive;
-  }
-
-  //tra ve tong so lan vesting
-  function getTimes() public view virtual returns (uint256) {
-    return _times;
-  }
-
-  //tra ve thoi gian va ti le ung voi tung lan vesting
-  function getRateAndTimeLockForTimes(uint256 times_)
-    public
-    view
-    virtual
-    returns (timesAndRate memory)
-  {
-    return _timesAndRate[times_];
-  }
-
-  // tra ve amount con lai
-  function getAmountRemaining(address beneficiary_)
-    public
-    view
-    virtual
-    returns (uint256)
-  {
-    return
-      _amount[beneficiary_].amountReceive -
-      _amount[beneficiary_].amountReceived;
-  }
-
-  // tra ve tong amount
-  function getSumAmount() public view virtual returns (uint256) {
-    return _sumAmount;
-  }
-
-  /*
-    - truoc khi goi ham release() admin phai transfer 1 luong token cho address timeLock
-    - token.balanceOf(address(this)) phai >= tong cung vesting * ti le vestin cua vong do
-    - sau khi release, vestor tu transferFrom ve vi cua minh
-    */
-
-  //admin goi khi het thoi gian lock, de chuyen approve cho vestor
-  function release() public virtual onlyOwner {
+/**
+  * approve token cho vestor theo ti le vesting
+ */
+  function release() public onlyOwner {
     require(block.timestamp >= _timeStart, "not time release");
     require(_status == true, "time error");
-
-    uint256 amount = token().balanceOf(address(this));
-    require(
-      amount >=
-        SafeMath.div(SafeMath.mul(_sumAmount, _timesAndRate[_count].rate), 100),
-      "not enough tokens to release"
-    );
 
     for (uint256 i = 0; i < _beneficiary.length; i++) {
       uint256 lastAmount = token().allowance(address(this), _beneficiary[i]);
       if (lastAmount != 0) {
         token().safeDecreaseAllowance(_beneficiary[i], lastAmount);
       }
-      if (_count == _times - 1) {
-        uint256 amountSend = _amount[_beneficiary[i]].amountReceive -
-          _amount[_beneficiary[i]].amountReceived;
-        token().safeApprove(_beneficiary[i], lastAmount + amountSend);
-      } else {
-        uint256 amountSend = SafeMath.div(
-          SafeMath.mul(
-            _amount[_beneficiary[i]].amountReceive,
-            _timesAndRate[_count].rate
-          ),
-          100
-        );
-        token().safeApprove(_beneficiary[i], lastAmount + amountSend);
-        _amount[_beneficiary[i]].amountReceived += amountSend;
-      }
+      uint256 amountSend = SafeMath.div(
+        SafeMath.mul(
+          _amount[_beneficiary[i]].amountReceive,
+          _timesAndRate[_count].rate
+        ),
+        100
+      );
+      token().safeApprove(_beneficiary[i], lastAmount + amountSend);
+      _amount[_beneficiary[i]].amountReceived += amountSend;
     }
+    _sumBalanceContractInTimes =
+      _sumBalanceContractInTimes +
+      SafeMath.div(SafeMath.mul(_sumAmount, _timesAndRate[_count].rate), 100);
     _count++;
     _status = false;
   }
 
-  //kiem tra thoi diem release
+/**
+  * reset cac bien contract ve ban dau
+ */
+  function resetData() public onlyOwner {
+    require(_count >= _times, "not time to reset data");
+    delete _beneficiary;
+    _times = 0;
+    _statusContract = 2;
+    _sumBalanceContractInTimes = 0;
+    _count = 0;
+    _sumAmount = 0;
+  }
+
+/**
+  * approve token con lai cho owner
+ */
+  function withdrawCoin() public onlyOwner returns (uint256) {
+    uint256 amount = token().balanceOf(address(this));
+    require(amount != 0, "not have token");
+    uint256 amountRevert = amount - _sumBalanceContractInTimes;
+    token().safeApprove(owner(), amountRevert);
+    return amountRevert;
+  }
+
+/**
+  * kiem tra thoi diem realease
+ */
   function checkTimeRelease() public view returns (bool) {
     if (_status == false) {
       return false;
@@ -190,5 +171,12 @@ contract TokenTimelock is Ownable {
       return true;
     }
     return false;
+  }
+
+/**
+  * tra ve dia chi token erc20
+ */
+  function token() public view returns (IERC20) {
+    return _token;
   }
 }
