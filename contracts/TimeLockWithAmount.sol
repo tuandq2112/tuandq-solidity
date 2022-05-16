5724f561f86d45afea70731c8d8fe16c549ca812
tuandq <doquoctuan311@gmail.com>
2022-05-16 13:26:10 +0700

    ISCHAIN-170 Sửa timelock smart contract + comment code

--- contracts/TimeLockWithAmount.sol
+++ contracts/TimeLockWithAmount.sol
@@ -12,46 +12,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract TimeLockWithAmount is Ownable {
   event ClaimPrivateToken(uint256 _amount, address _owner, uint256 _time);
   event ClaimPublicToken(uint256 _amount, address _owner, uint256 _time);
+
   struct TimeAndAmount {
     uint256 time;
     uint256 amount;
   }
   uint256 private FRACTIONS = 10**uint256(18);
-
-  function _addToList(
-    TimeAndAmount[] storage listTimeAndAmount,
-    uint256[] memory listTime,
-    uint256[] memory listAmount
-  ) private {
-    require(
-      listTime.length == listAmount.length,
-      "Number of time is not equal number of amount"
-    );
-    uint256 countTime = block.timestamp;
-    for (uint256 i = 0; i < listTime.length; i++) {
-      countTime += listTime[0];
-      listTimeAndAmount.push(
-        TimeAndAmount(countTime, listAmount[i] * FRACTIONS)
-      );
-    }
-  }
-
-  function _countAmountByTime(TimeAndAmount[] memory listTimeAndAmount)
-    private
-    view
-    returns (uint256)
-  {
-    uint256 totalAmount = 0;
-    uint256 currentTime = block.timestamp;
-    for (uint256 i = 0; i < listTimeAndAmount.length; i++) {
-      if (listTimeAndAmount[i].time <= currentTime) {
-        totalAmount += listTimeAndAmount[i].amount;
-      } else {
-        break;
-      }
-    }
-    return totalAmount;
-  }
 
   IERC20 private _token;
 
@@ -59,9 +25,23 @@ contract TimeLockWithAmount is Ownable {
 
   TimeAndAmount[] private _listPrivateTimeAndAmount;
 
-  ///@notice tokens received
   uint256 private _receivedPublicToken = 0;
+
   uint256 private _receivedPrivateToken = 0;
+
+  /**
+   * @dev Sets the values for {listPublicTimeAndAmount} and {listPrivateTimeAndAmount}.
+   *
+   * The default value of {decimals} is 18. To select a different value for
+   * {decimals} you should overload it.
+   *
+   * All two of these values are immutable: they can only be set once during
+   * construction.
+   * Requirements:
+   *
+   * - `length of listPublicAmount_ and length of listPublicTime_` can be equal
+   * - `length of listPrivateAmount_ and length of listPrivateTime_` can be equal
+   */
 
   constructor(
     address tokenAddress_,
@@ -81,8 +61,16 @@ contract TimeLockWithAmount is Ownable {
       "Parameters invalid"
     );
     _token = IERC20(tokenAddress_);
-    _addToList(_listPublicTimeAndAmount, listPublicTime_, listPublicAmount_);
-    _addToList(_listPrivateTimeAndAmount, listPrivateTime_, listPrivateAmount_);
+    _initTimeAndRatio(
+      _listPublicTimeAndAmount,
+      listPublicTime_,
+      listPublicAmount_
+    );
+    _initTimeAndRatio(
+      _listPrivateTimeAndAmount,
+      listPrivateTime_,
+      listPrivateAmount_
+    );
   }
 
   ///@notice require before constructor
@@ -104,16 +92,65 @@ contract TimeLockWithAmount is Ownable {
       _tokenAddress != address(0);
   }
 
-  ///@notice get token over time
-  function _getPrivateTokensCanClaim() private view returns (uint256) {
+  /**
+   * @dev Corresponding to a time there will be a price we push into the array.
+   */
+
+  function _initTimeAndRatio(
+    TimeAndAmount[] storage _listTimeAndAmount,
+    uint256[] memory _listTime,
+    uint256[] memory _listAmount
+  ) private {
+    require(
+      _listTime.length == _listAmount.length,
+      "Number of time is not equal number of amount"
+    );
+    uint256 countTime = block.timestamp;
+    for (uint256 i = 0; i < _listTime.length; i++) {
+      countTime += _listTime[0];
+      _listTimeAndAmount.push(
+        TimeAndAmount(countTime, _listAmount[i] * FRACTIONS)
+      );
+    }
+  }
+
+  /**
+   * @dev Get all element have time < now and count all amount of this .
+   */
+  function _countAmountByTime(TimeAndAmount[] memory _listTimeAndAmount)
+    private
+    view
+    returns (uint256)
+  {
+    uint256 totalAmount = 0;
+    uint256 currentTime = block.timestamp;
+    for (uint256 i = 0; i < _listTimeAndAmount.length; i++) {
+      if (_listTimeAndAmount[i].time <= currentTime) {
+        totalAmount += _listTimeAndAmount[i].amount;
+      } else {
+        break;
+      }
+    }
+    return totalAmount;
+  }
+
+  /**
+   * @dev Get total private amount unlocked for user .
+   */
+  function _getPrivateTokensUnlocked() private view returns (uint256) {
     return _countAmountByTime(_listPrivateTimeAndAmount);
   }
 
-  ///@notice get token over time
-  function _getPublicTokensCanClaim() private view returns (uint256) {
+  /**
+   * @dev Get total public amount unlocked for user .
+   */
+  function _getPublicTokensUnlocked() private view returns (uint256) {
     return _countAmountByTime(_listPublicTimeAndAmount);
   }
 
+  /**
+   * @dev Get milestone and amount of private release.
+   */
   function getPublicTimeAndAmount()
     public
     view
@@ -122,6 +159,9 @@ contract TimeLockWithAmount is Ownable {
     return _listPublicTimeAndAmount;
   }
 
+  /**
+   * @dev Get milestone and amount of public release.
+   */
   function getPrivateTimeAndAmount()
     public
     view
@@ -130,9 +170,14 @@ contract TimeLockWithAmount is Ownable {
     return _listPrivateTimeAndAmount;
   }
 
-  ///@notice release
+  /**
+   * @dev User claim token.
+   * Requirements:
+   *
+   * - Amount less than or equal token unlocked minus token user received
+   */
   function releasePublicToken(uint256 amount) public onlyOwner {
-    uint256 tokenPublicCanClaim = _getPublicTokensCanClaim();
+    uint256 tokenPublicCanClaim = _getPublicTokensUnlocked();
     require(
       tokenPublicCanClaim - _receivedPublicToken >= amount,
       "Exceed total private token"
@@ -142,9 +187,14 @@ contract TimeLockWithAmount is Ownable {
     emit ClaimPrivateToken(amount, owner(), block.timestamp);
   }
 
-  ///@notice release
+  /**
+   * @dev User claim token.
+   * Requirements:
+   *
+   * - Amount less than or equal token unlocked minus token user received
+   */
   function releasePrivateToken(uint256 amount) public onlyOwner {
-    uint256 tokenPrivateCanClaim = _getPrivateTokensCanClaim();
+    uint256 tokenPrivateCanClaim = _getPrivateTokensUnlocked();
     require(
       tokenPrivateCanClaim - _receivedPrivateToken >= amount,
       "Exceed total private token"
@@ -154,13 +204,17 @@ contract TimeLockWithAmount is Ownable {
     emit ClaimPublicToken(amount, owner(), block.timestamp);
   }
 
-  ///@notice public get token over time
+  /**
+   * @dev Total public token user can claims.
+   */
   function getPublicTokenCanClaim() public view returns (uint256) {
-    return _getPublicTokensCanClaim();
+    return _getPublicTokensUnlocked() - _receivedPublicToken;
   }
 
-  ///@notice public get token over time
+  /**
+   * @dev Total private token user can claims.
+   */
   function getPrivateTokenCanClaim() public view returns (uint256) {
-    return _getPrivateTokensCanClaim();
+    return _getPrivateTokensUnlocked() - _receivedPrivateToken;
   }
 }
--- migrations/8_timelock_amount.js
+++ migrations/8_timelock_amount.js
@@ -1,18 +1,18 @@
 const TimeLockWithAmount = artifacts.require("TimeLockWithAmount");
 const oneMonth = 60 * 60 * 24 * 30;
-const oneCoin = Math.pow(10, 10);
+const oneMinute = 60;
 module.exports = function (deployer) {
   deployer.deploy(
     TimeLockWithAmount,
     "0xD205C35e8667E2b63eb43388f154bfc22f48Abe1",
-    [1000, 2000, 3000, 1000, 3500, 5222],
+    [1000, 2000, 3000, 1000, 3500, 1000],
     [
-      oneMonth * 3,
-      oneMonth * 3,
-      oneMonth * 6,
-      oneMonth * 9,
-      oneMonth * 12,
-      oneMonth * 6,
+      oneMinute * 5,
+      oneMinute * 10,
+      oneMinute * 15,
+      oneMinute * 20,
+      oneMinute * 25,
+      oneMinute * 30,
     ],
     [3000, 1000, 3500, 1000, 2000],
     [oneMonth * 1, oneMonth * 1, oneMonth * 6, oneMonth * 3, oneMonth * 18]
